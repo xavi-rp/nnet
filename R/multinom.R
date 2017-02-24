a2bb1ac902e1e40775f3db8af4b31b27a3363490
xavi-rp <xavi.rotllan.puig@gmail.com>
2017-02-24 12:42:44 +0100

    no message

--- R/multinom.R
+++ R/multinom.R
@@ -199,23 +199,11 @@ predict.multinom <- function(object, newdata, type = c("class_constr", "class", 
           print("predicting using predict.multinom() including constraints (max-min of each choice at each variable)")  
           
       Y <- as.data.frame(Y)
-      print("head(Y")
-      print(head(Y))
-      print("names(Y)")
-      print(names(Y))
-      
+
           rgsrs <- regressors[!regressors %in% c(regressors[1], regressors[length(regressors)])]  # this is for our particular case. We need to remove first (column of choices) and last (categorical) elements 
-          print("rgsrs")
-          print(rgsrs)
-          
           n <- ncol(Y)
-          print("n")
-          print(n)
-          
+
           Y1 <- factor(levels=seq_along(object$lev), labels=object$lev)
-          print("head(Y1")
-          print(head(Y1))
-          
 
           for(cs in 1:nrow(newdata)){ 
             rnd <- 0
